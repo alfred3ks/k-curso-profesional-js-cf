efbf058f5edd4a627578305499447245215cd18b
Alfredo Sánchez <alfred3ks@gmail.com>
2022-06-01 18:59:55 +0200

    Undefined, null, NaN y infinity.

---
+++ undefined-null-nan-infinity.js
@@ -0,0 +1,27 @@
+/*
+
+Vamos a ver el tema de undefined, null, NaN y infinity. Son unos tipos de datos especiales.
+
+undefined: Es una variable.
+El tipo de dato undefined, es cuando tenemos declarada a una variable pero no se le ha asignado ningun valor.
+
+let nombre;
+console.log(typeof nombre); undefined.
+
+null: Es un objeto
+Cuando hablamos de null estamos hablando de un objeto especial de JS que indica la ausencia de valor.
+
+let saldo = null;
+console.log(typeof saldo); object
+
+NaN: Not a number
+let operacion = "hola" * 3;
+console.log(operacion); NaN
+
+infinity:
+Ahora veremos un objeto llamado infinity a diferencia de otros lenguajes en JS dividor por cero no da error, da infinito
+
+let division = 100/0;
+console.log(division); infinity
+
+*/
