9cdff200473cffaa2d455abb08d8f6bc78d971fa
Alfredo Sánchez <alfred3ks@gmail.com>
2022-06-10 19:50:14 +0200

    Metodos call(), apply(), bind() para el contexto.

---
+++ 5-el-contexto/39-bind-call-apply-metodos-contexto.js
@@ -0,0 +1,38 @@
+/*
+
+Existen unos metodos por medio de los cuales podemos asignarle el valor de this.
+
+- bind()
+-call()
+-apply()
+
+Los metodos que modifican el contexto y que se ejecutan de manera inmediata la funcion:
+call()
+apply
+
+Los que construyen una nueva funcion para ejecutarla:
+bind()
+
+
+*/
+
+function Estudiantes() {
+    this.nombre = "Ursula",
+    this.saludar = function() {
+            console.log("Hola " + this.nombre, arguments);
+        }
+    // this.saludar = this.saludar.bind(this);
+}
+
+let e = new Estudiantes();
+e.saludar();
+
+// call():
+e.saludar.call({}, "1", "2");
+
+// apply():
+e.saludar.apply({},["3","4"]);
+
+// bind():
+let nueva = e.saludar.bind({}, "6", "7");
+nueva();
