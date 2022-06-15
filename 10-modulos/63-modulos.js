cfc9027dab17342d7d375ec9cac7c57bf05db88c
Alfredo Sánchez <alfred3ks@gmail.com>
2022-06-15 15:19:25 +0200

    Los modulos en JS.

---
+++ 10-modulos/63-modulos.js
@@ -0,0 +1,10 @@
+/*
+
+Los modulos en JS son archivos que nos permiten compartir elementos de codigo con otro modulos, de una manera muy simple podriamos decir que es una forma a traves de la cual podemos compartir variables, funciones, clases y otros elementos entre archivos de JS.
+
+Los modulos son una estrategia que usamos los programadores para dividir el codigo que desarrollamos, esa division se debe hacer de tal forma que cada uno de los elementos en los que estamos dividiendo el codigo sea reemplazable, removible, y facil de integrar con el proyecto.
+
+los modulos en JS son el resultado de una decada de trabajo durante la cual el equipo que mantiene la especificacion del lenguaje a buscado estandarizar una manera en que podamos separar el codigo en piezas que podamos quitar o poner en funcion de las necesidades.
+
+
+*/
