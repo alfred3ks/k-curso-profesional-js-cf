2bb09bb2aaa2d94124d475d42fc1f636ddd0bf9e
Alfredo Sánchez <alfred3ks@gmail.com>
2022-06-18 14:47:29 +0200

    Donde se usan los generadores e iteradores.

---
+++ 11-generadores-e-iteradores/73-donde-se-usan-los-generadores.js
@@ -0,0 +1,13 @@
+/*
+
+Los generadores y etiradores se usan mucho sobre todo en operaciones asincronas. Un buen ejemplo de quien usa generadores es la libreria co.js cuya sintaxis lo podemos ver en su repo.
+
+https://github.com/tj/co
+
+Tambien tenemos redux saga:
+
+https://github.com/redux-saga/redux-saga
+
+
+
+*/
